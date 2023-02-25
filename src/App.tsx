a1156a8215c1118956ad899461a8cd17b73bdede
Zakky Muhammad Fajar <zakky.muhammad@sicepat.com>
2023-02-25 12:22:22 +0700

    style: background and search bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Header } from "./components/Header";
 
 function App() {
   return (
-    <div className="container">
+    <div className=" bg-black px-20 pb-6">
       <Header />
       <FilterBox />
     </div>
